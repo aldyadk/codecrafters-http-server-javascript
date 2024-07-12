1b78191cbfad26651a141a4cad2ac9d0632b6f61
aldy.andika <aldy.andika@gmail.com>
2024-07-12 03:07:32 +0000

    change response depending on method & path

--- app/main.js
+++ app/main.js
@@ -5,9 +5,22 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-  socket.write('HTTP/1.1 200 OK\r\n\r\n')
   socket.on("close", () => {
     socket.end();
+  });
+
+  socket.on('data', (data) => {
+    const [request,headers,body] = data.toString().split('\r\n');
+    const [method, path, version] = request.split(' ');
+    const [headerName, headerValue] = headers.split(': ');
+    // console.log(`Received request: ${request}`);
+    // console.log(`headers: ${headers}`);
+    // console.log(`Received body: ${body}`);
+    if(method === 'GET' && path === '/') {
+      socket.write('HTTP/1.1 200 OK\r\n\r\n')
+    } else {
+      socket.write('HTTP/1.1 404 Not Found\r\n\r\n');
+    }
   });
 });
 
