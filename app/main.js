1220dc9e3003153c684bc1b92d5a9b53cae2b916
aldy.andika <aldy.andika@gmail.com>
2024-07-12 06:13:27 +0000

    test

--- app/main.js
+++ app/main.js
@@ -35,7 +35,9 @@ const server = net.createServer((socket) => {
             console.error(err)
             socket.write('HTTP/1.1 500 Internal Server Error\r\n\r\n');
           } else {
+            console.log(content)
             content = compressed;
+            console.log(content)
             socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n${encoding}\r\n${content}`)
           }
         })
