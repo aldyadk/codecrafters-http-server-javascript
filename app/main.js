bd9a0f8dbf697ed812a5494188fd074b8f9477d2
aldy.andika <aldy.andika@gmail.com>
2024-07-12 03:33:00 +0000

    handle echo

--- app/main.js
+++ app/main.js
@@ -9,15 +9,20 @@ const server = net.createServer((socket) => {
     socket.end();
   });
 
+  const endingConnection = () => socket.end()
+
   socket.on('data', (data) => {
-    const [request,headers,body] = data.toString().split('\r\n');
+    const [request, headers, body] = data.toString().split('\r\n');
     const [method, path, version] = request.split(' ');
+    const [path0, pathA, pathB, ...subPath] = path.split('/');
     const [headerName, headerValue] = headers.split(': ');
     // console.log(`Received request: ${request}`);
     // console.log(`headers: ${headers}`);
     // console.log(`Received body: ${body}`);
-    if(method === 'GET' && path === '/') {
+    if (method === 'GET' && path === '/') {
       socket.write('HTTP/1.1 200 OK\r\n\r\n')
+    } else if (method === 'GET' && pathA === 'echo' && !!pathB) {
+      socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${pathB.length}\r\n\r\n${pathB}`)
     } else {
       socket.write('HTTP/1.1 404 Not Found\r\n\r\n');
     }
