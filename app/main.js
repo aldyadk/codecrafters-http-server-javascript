e8aa42cd4e211ac3832dac3e4847ae9c71d2b6e6
aldy.andika <aldy.andika@gmail.com>
2024-07-12 06:52:17 +0000

    test

--- app/main.js
+++ app/main.js
@@ -41,6 +41,7 @@ const server = net.createServer((socket) => {
             socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n${encoding}\r\n`)
             socket.write(content)
             socket.end()
+            console.log('end')
           }
         })
       } else {
